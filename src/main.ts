c30345544cfcff0e7098a8e0988a5aaf66d5f052
luongminhanh <luongminhanh.haui@gmail.com>
2024-12-17 22:58:19 +0700

    get variable from env

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  const configService = app.get(ConfigService);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();

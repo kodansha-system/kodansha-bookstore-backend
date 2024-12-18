63b9aa11ace8108af7e252b92eef38d4f645729d
luongminhanh <luongminhanh.haui@gmail.com>
2024-12-18 09:10:44 +0700

    add user schema

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop()
+  created_at: Date;
+
+  @Prop()
+  updated_at: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);

204e2ab300853d3fa8ff16464d7d6aeec816e5cf
luongminhanh <luongminhanh.haui@gmail.com>
2025-02-04 09:37:11 +0700

    get company by id

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -45,7 +45,7 @@ export class CompaniesController {
   @Public()
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.companiesService.findOne(+id);
+    return this.companiesService.findOne(id);
   }
 
   @Put(':id')
